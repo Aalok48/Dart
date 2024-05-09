1249221c803b00d86a385637a7c83ce14b08ced1
Alok <karnaalok48@gmail.com>
2024-05-09 23:14:06 +0545

    if else statements

---
+++ ifelse.dart
@@ -0,0 +1,8 @@
+void main() {
+  int salary = 45000;
+  if (salary > 40000) {
+    print("Hello there, you are getting a smart salary.");
+  } else {
+    print("Fuck you. How are you surviving with something this much less.");
+  }
+}
