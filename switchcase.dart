217b99a43a323d86b6fb0e4bc71e4fb2d74ae95c
Alok <karnaalok48@gmail.com>
2024-05-09 23:38:17 +0545

    switch case

---
+++ switchcase.dart
@@ -0,0 +1,22 @@
+void main() {
+  var grade = 'D';
+  switch (grade) {
+    case 'A':
+      print("Excellant Grade");
+      break;
+    case 'B':
+      print('Good Grade');
+      break;
+    case 'C':
+      print('Try to get better');
+      break;
+    case 'D':
+      print('Ahh, you piece of SHIT');
+      break;
+    default:
+      print(("Grade not found..."));
+  }
+}
+
+
+// Only integer and string variables can be used with Switch statement
