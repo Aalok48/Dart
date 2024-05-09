2a13d300c32cc784e9c4c98786b8a4525d096ba6
Alok <karnaalok48@gmail.com>
2024-05-09 23:17:46 +0545

    if ladder created

---
+++ ifladder.dart
@@ -0,0 +1,10 @@
+void main() {
+  var marks = 80;
+  if (marks >= 80 && marks <= 100) {
+    print("Congratulation!!! You got a Grade A");
+  } else if (marks >= 80 && marks <= 80) {
+    print("You got a Grade B");
+  } else {
+    print("Fuck You");
+  }
+}
