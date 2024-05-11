ced4610a40ac84832055554f4b6bbf601e782e94
Alok <karnaalok48@gmail.com>
2024-05-11 14:07:50 +0545

    for loop syntax

---
+++ forloop.dart
@@ -0,0 +1,14 @@
+// for loop in dart
+
+void main() {
+  // syntax of FOR loop in dart
+  for (var i = 0; i <= 9; i++) {
+    print(i);
+  }
+
+  // syntax of for in loop in dart
+  var word = 'DART';
+  for (var j in word.split('')) {
+    print(j);
+  }
+}
