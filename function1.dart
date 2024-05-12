05e22626642f32b9c999b4e83ddd58d18cbf8b07
Alok <karnaalok48@gmail.com>
2024-05-12 22:09:07 +0545

    function properties

---
+++ function1.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  int length = 10, breadth = 5, area;
+  area = find_area(length, breadth);
+  print('The area of the reactangle is $area');
+}
+
+int find_area(int length, int breadth) {
+  return length * breadth;
+}
