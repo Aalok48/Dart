3da189e1a674ad34194479c0c5bf0ef666f4a919
Alok <karnaalok48@gmail.com>
2024-05-24 14:43:14 +0545

    Higher order function in dart

---
+++ higherorderfunc.dart
@@ -0,0 +1,24 @@
+// higher order function can
+// 1. accept function as parameter
+// 2. return a function
+// 3. do both
+
+// ignore_for_file: unused_element
+
+void main() {
+  Function addtwonumber = (int a, int b) => a + b;
+  someotherfunction("Hello", addtwonumber);
+  var myfunc = tasktoperform();
+  print(myfunc(10));
+}
+
+void someotherfunction(String message, Function myfunc) {
+  print(message);
+  myfunc(2, 4);
+}
+
+// Higher order function
+Function tasktoperform() {
+  Function multiplefour = (int x) => x * 4;
+  return multiplefour;
+}
